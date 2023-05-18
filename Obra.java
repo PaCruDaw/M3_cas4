c3245c3165b9655af68b8f134c147d43e46efed0
michipce <michipce@gmail.com>
2023-05-18 23:05:42 +0200

    alguns comentaris

--- Obra.java
+++ Obra.java
@@ -6,6 +6,13 @@ public class Obra {
     private String autor;
 
     //Constructor amb tots els parametres
+    /**
+     * Constuctor
+     * @param titulo title of representation
+     * @param autora author of representation
+     * @param duracion duration of the performance
+     * @param edadmin age min for viewer
+     */
     public Obra (String titulo,String autora, short duracion, byte edadmin) {
         this.titol = titulo;
         this.autor = autora;
@@ -13,55 +20,78 @@ public class Obra {
         this.peggi = edadmin;
     }
 
-    public Obra (String titulo,String autora, short duracion) {
-        this.titol = titulo;
-        this.autor = autora;
-        this.durada = duracion;
-    }
 
     //Constructor vuit per poder intciar sessio
+    /**
+     * Constructor
+     */
     public Obra () {
 
     }
 
-   /*  public Obra (Obra repre) {
-        this.titol = repre.titol;
-        this.peggi = repre.peggi;
-        this.durada = repre.durada;
-        this.autor = repre.autor;
-    } */
-
     //SETTERS
+    /**
+     * setter for tittle
+     * @param titulo title of representation
+     */
     public void setTitol (String titulo) {
         this.titol = titulo;
     }
 
+    /**
+     * setter for peggi
+     * @param edadMin age min for viewer
+     */
     public void setPeggi (byte edadMin) {
         this.peggi = edadMin;
     }
 
+    /**
+     * setter for durada
+     * @param duracion duration of the performance
+     */
     public void setDurada (short duracion) {
         this.durada = duracion;
     }
 
-    public void setAutor (String autorp) {
-        this.autor = autorp;
+    /**
+     * setter for autor
+     * @param autor author of representation
+     */
+    public void setAutor (String autor) {
+        this.autor = autor;
     }
 
 
     //GETTERS
+    /**
+     * getter for title
+     * @return title of representation
+     */
     public String getTitul () {
         return titol;
     }
 
+    /**
+     * getter for peggi
+     * @return age min for viewer
+     */
     public byte getPeggi () {
         return peggi;
     }
 
+    /**
+     * getter for durada
+     * @return duration of the performance
+     */
     public short getDurada () {
         return durada;
     }
 
+    /**
+     * getter for autor
+     * @return author of representation
+     */
     public String getAutor () {
         return autor;
     }
